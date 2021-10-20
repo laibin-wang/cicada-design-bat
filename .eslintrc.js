b4dbea5d27cd67fda3a8bf8b7abd8d69168b9e66
王来彬 <wanglb@idss-cn.com>
2021-10-20 10:35:56 +0800

    chore(项目配置文件): 项目配置文件
    项目配置文件

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'plugin:vue/vue3-essential',
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+    'plugin:prettier/recommended',
+  ],
+  parserOptions: {
+    ecmaVersion: 13,
+    parser: '@typescript-eslint/parser',
+    sourceType: 'module',
+  },
+  plugins: ['vue', '@typescript-eslint'],
+  rules: {
+    'no-multi-spaces': [2, { ignoreEOLComments: true }],
+    'eol-last': 0,
+  },
+}
